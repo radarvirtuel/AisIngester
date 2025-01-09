6ea44c49b0a343d713b094d730f38bca23d6ed28
Laurent Duval <laurent.duval@adsbnetwork.com>
2025-01-09 20:15:23 +0100

    Sauvegarde des modifications locales

--- app/src/decorators/ais-validator/ais-validator.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { applyDecorators, UseGuards } from '@nestjs/common';
-import { AisValidatorGuard } from 'src/guards/ais-validator/ais-validator.guard';
-
-export function AisValidator() {
-  return applyDecorators(UseGuards(AisValidatorGuard));
-}
--- app/src/types/array.d.ts
+++
@@ -1,4 +0,0 @@
-interface Array<T> {
-  asyncForEach(this: T[], callback: (value: T, index: number, array: T[]) => Promise<void>): Promise<void>;
-  shuffle(this: T[]): T[];
-}
